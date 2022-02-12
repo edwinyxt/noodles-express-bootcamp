3ac4fb6dbea15fcbc0836d6775d86cb16154146f
edwinyxt <yixiongtay@gmail.com>
2022-02-12 09:32:58 +0800

    post-class

--- index.js
+++ index.js
@@ -1,1 +1,39 @@
-console.log('hello world');
+import express from "express";
+import { read } from "/home/edwinyxt/bootcamp/module2/29-Jan/to-do-json/jsonFileStorage.js";
+const app = express();
+
+const getRecipeByIndex = (request, response) => {
+  read("data.json", (err, data) => {
+    // Respond with the name at the index specified in the URL
+
+    const content = data.recipes[request.params.index];
+    if (content) {
+      response.send(content);
+    } else {
+      response.status(404).send("sorry, we cannot find that!");
+    }
+  });
+};
+
+const getRecipeByYield = (request, response) => {
+  read("data.json", (err, data) => {
+    // Respond with the name at the index specified in the URL
+
+    const list = data.recipes;
+    const portion = Number(request.params.portions);
+    console.log(portion);
+    const result = list.filter((item) => item["yield"] === portion);
+
+    if (result) {
+      response.send(result);
+    } else {
+      response.status(404).send("sorry, we cannot find that!");
+    }
+  });
+};
+
+app.get("/recipe/:index", getRecipeByIndex);
+
+app.get("/yield/:portions", getRecipeByYield);
+
+app.listen(3004);
